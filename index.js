580a0262822ca957b5470d8ba7aadab9721616a4
asmraihan <asmraihanctg14@gmail.com>
2023-06-05 01:31:59 +0600

    booking delete api

--- index.js
+++ index.js
@@ -91,11 +91,45 @@ async function run() {
       res.send(update)
     })
 
+
+    // // Get all bookings for guest
+    // app.get('/bookings/:email', async(req,res)=>{
+    //   const email = req.params.email
+    //   if(!email){
+    //     res.send([])
+    //   }
+    //   const query = {'guest.email': email} /* to reach bookings>guest>email in db */
+    //   const result = await bookingsCollection.find(query).toArray()
+    //   // console.log('email got',result)
+    //   res.send(result)
+    // })
+
+      // Get bookings for guest
+      app.get('/bookings', async (req, res) => {
+        const email = req.query.email
+  
+        if (!email) {
+          res.send([])
+        }
+        const query = { 'guest.email': email }
+        const result = await bookingsCollection.find(query).toArray()
+        res.send(result)
+      })
+
+
     // save a booking in db
     app.post('/bookings', async (req, res) => {
       const booking = req.body
       console.log(booking)
       const result = await bookingsCollection.insertOne(booking)
+      res.send(result)
+    })
+
+    // delete a booking from db
+    app.delete('/bookings/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingsCollection.deleteOne(query)
       res.send(result)
     })
 
